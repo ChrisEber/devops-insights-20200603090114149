4429e3c7815fc000cbde14e1e412b2eb159974d1
Chris E <chrisebert18@gmail.com>
2020-06-04 11:03:33 +0000

    0.9.9.5

--- src/components/Zip.js
+++ src/components/Zip.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import res from `../containers/AppContainer`;
 
 function Zip(props) {
 
@@ -11,7 +12,7 @@ function Zip(props) {
             setValidationError('* should be a valid place name');
             props.clearResponse();
         } else {
-            setValidationError(' ');
+            setValidationError('res');
             props.onZipChange(event.target.value);
         }
     };
